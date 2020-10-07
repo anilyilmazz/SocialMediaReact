353f2fd00b3ac864df9999371f87ebd0a706257f
Anil <incelefon@gmail.com>
2020-10-07 22:05:44 +0300

    Grid system for mobile

--- src/components/theme/Home.js
+++ src/components/theme/Home.js
@@ -10,13 +10,12 @@ function Home() {
     return (
         <div>
             <NotificationContainer />
-            <div className='col-10 mx-auto row mt-2'>
-                <div className='col-8'>
+            <div className='col-12 col-sm-12 col-md-12 col-lg-10 mx-auto row mt-2'>
+                <div className='col-12 col-sm-12 col-md-12 col-lg-8'>
                     <CreatePost/>
                     <PostsList/>
                 </div>
-                <div className="vl"></div>
-                <div className='col-3'>
+                <div className='col-12 col-sm-12 col-md-12 col-lg-3'>
                     <RigthSideBar />
                 </div>
             </div>
